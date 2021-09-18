4371fd37f5fb8f908b4d1e6d74b9bdc72fbc3f7b
Cindy <cindy@Students-MacBook-Pro.local>
2021-09-17 18:04:27 -0600

    created Spring Boot MVC project

---
+++ MyPokedex/src/main/java/com/skilldistillery/jpacrud/MyPokedexApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPokedexApplication.class, args);
+	}
+
+}
